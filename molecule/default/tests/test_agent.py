70975bbf262553c1d207f52b224272a6bde8c1a5
Werner Dijkerman <werner.dijkerman@iwelcome.com>
2020-08-31 16:35:59 +0200

    Made it work with Github Actions

--- molecule/default/tests/test_agent.py
+++ molecule/default/tests/test_agent.py
@@ -6,35 +6,35 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('agent')
 
 
-def test_ossec_package_installed(Package, SystemInfo):
-    if SystemInfo.distribution in ['debian', 'ubuntu']:
-        ossec = Package('ossec-hids-agent')
-    elif SystemInfo.distribution == 'centos':
-        ossec = Package('ossec-hids')
+def test_ossec_package_installed(host):
+    if host.system_info.distribution in ['debian', 'ubuntu']:
+        ossec = host.package('ossec-hids-agent')
+    elif host.system_info.distribution == 'centos':
+        ossec = host.package('ossec-hids')
     assert ossec.is_installed
 
 
-def test_ossec_service_running_and_enabled(Service, SystemInfo):
-    if SystemInfo.distribution in ['debian', 'ubuntu']:
-        ossec = Service('ossec')
+def test_ossec_service_running_and_enabled(host):
+    if host.system_info.distribution in ['debian', 'ubuntu']:
+        ossec = host.service('ossec')
     else:
-        ossec = Service('ossec-hids')
+        ossec = host.service('ossec-hids')
     assert ossec.is_enabled
     assert ossec.is_running
 
 
-def test_client_keys(File):
-    zabbix_server_conf = File("/var/ossec/etc/client.keys")
+def test_client_keys(host):
+    zabbix_server_conf = host.file("/var/ossec/etc/client.keys")
     assert zabbix_server_conf.user == "root"
     assert zabbix_server_conf.group == "ossec"
     assert zabbix_server_conf.mode == 0o640
 
 
-def test_ossec_agent(File, SystemInfo):
-    if SystemInfo.distribution in ['debian', 'ubuntu']:
-        ossec_file = File("/var/ossec/etc/ossec.conf")
+def test_ossec_agent(host):
+    if host.system_info.distribution in ['debian', 'ubuntu']:
+        ossec_file = host.file("/var/ossec/etc/ossec.conf")
     else:
-        ossec_file = File("/var/ossec/etc/ossec-agent.conf")
+        ossec_file = host.file("/var/ossec/etc/ossec-agent.conf")
     assert ossec_file.user == "root"
     assert ossec_file.group == "root"
     assert ossec_file.mode == 0o644
